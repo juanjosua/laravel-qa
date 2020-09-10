c2dddbd6446f021c12d591d49a93a4bb6a220066
Juan Josua <juan_josua@apps.ipb.ac.id>
2020-09-10 17:07:43 +0700

    Display all questions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', [Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('questions', Controllers\QuestionsController::class);
